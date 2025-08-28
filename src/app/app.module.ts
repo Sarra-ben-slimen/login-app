90dbb9324365b109bce3346a948f49ef223dc6ae
Sarra Ben Slimen <sarrabenslimen21@gmail.com>
2025-08-28 01:18:15 +0100

    page login template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login/login.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes =
+[
+    { path: '', redirectTo: 'login', pathMatch: 'full' }, //default route
+{path:'login',component:LoginComponent}  
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
