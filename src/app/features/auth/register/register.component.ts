277a9753bc062da2a1ac7013c04af176298df2a9
Sarra Ben Slimen <sarrabenslimen21@gmail.com>
2025-09-17 01:26:41 +0100

    add the register component

--- src/app/features/auth/register/register.component.ts
+++ src/app/features/auth/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { AuthService } from '../services/auth.service';
 
 @Component({
@@ -9,18 +9,34 @@ import { AuthService } from '../services/auth.service';
 })
 export class RegisterComponent {
 
+  showPassword: boolean = false;
+  showRepeatedPassword: boolean = false;
+
   registreForm : FormGroup;
   error = '';
   constructor(private fb : FormBuilder, private authService:AuthService)
   {
-    this.registreForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required],
-      repeatedPassword: ['',Validators.required]
-    })
+    this.registreForm = this.fb.group(
+      {
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(6), passwordStrengthValidator ]],
+        repeatedPassword: ['', Validators.required],
+      },
+      { validators: this.passwordMatchValidator } 
+    );
   }
 
+  passwordMatchValidator(control: AbstractControl) {
+    const password = control.get('password')?.value;
+    const repeatedPassword = control.get('repeatedPassword')?.value;
+    return password === repeatedPassword ? null : { mismatch: true };
+  };
   onRegistre() {
+    if (this.registreForm.invalid) {
+      this.registreForm.markAllAsTouched();
+      console.log('test test');
+      return;
+    }
     if (this.registreForm.valid) {
       const { email, password, repeatedPassword } = this.registreForm.value;
       this.authService.register(email,password,repeatedPassword).subscribe({
@@ -35,3 +51,16 @@ export class RegisterComponent {
   }
 
 }
+export const passwordStrengthValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+  const value = control.value;
+  if (!value) return null;
+
+  const hasUpperCase = /[A-Z]/.test(value);
+  const hasNumber = /\d/.test(value);
+  const hasSpecialChar = /[!@#$%^&*]/.test(value);
+  const minLength = value.length >= 6;
+
+  const valid = hasUpperCase && hasNumber && hasSpecialChar && minLength;
+
+  return valid ? null : { weakPassword: true };
+};
--- src/app/shared/components/input-field/input-field/input-field.component.ts
+++ src/app/shared/components/input-field/input-field/input-field.component.ts
@@ -1,12 +1,44 @@
-import { Component, Input } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { Component, forwardRef, Input } from '@angular/core';
+import { FormControl, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-input-field',
   templateUrl: './input-field.component.html',
-  styleUrls: ['./input-field.component.css']
+  styleUrls: ['./input-field.component.css'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputFieldComponent),
+      multi: true
+    }
+  ]
 })
 export class InputFieldComponent {
   @Input() type = 'text';
+  value: any;
+
   @Input() control :any;
+  disabled = false;
+
+  onChange = (_: any) => { };
+  onTouched = () => { };
+
+  writeValue(obj: any): void {
+    this.value = obj;
+  }
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+
+  updateValue(value: any) {
+    this.value = value;
+    this.onChange(value);
+    this.onTouched();
+  }
 }
