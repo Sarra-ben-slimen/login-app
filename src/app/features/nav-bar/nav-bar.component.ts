9c0470eb6265526747d4421e5b9a9e435654fa04
Sarra Ben Slimen <sarrabenslimen21@gmail.com>
2025-09-24 23:57:49 +0100

    add logout

--- src/app/features/nav-bar/nav-bar.component.ts
+++ src/app/features/nav-bar/nav-bar.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit {
   connected = false;
-
-  
+  token:string |null=null;
+  constructor(private authService: AuthService,private router:Router) { }
+  ngOnInit() :void {
+    this.token = this.authService.getToken();
+    this.connected == !!this.token;
+  }
+  logout(): void {
+    this.authService.removeToken();
+    this.token = null;
+    this.connected = false;
+    this.router.navigate(['/login']);  
+  }
 }
